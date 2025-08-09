d719ff3e55b0982bc8c43d34e3bc597ae605ae68
Stephen Yang <isstephen@gmail.com>
2025-08-09 12:54:17 +1200

    update groovy

--- dsl/test_adhoc_lambda.groovy
+++ dsl/test_adhoc_lambda.groovy
@@ -10,15 +10,18 @@ folder('lambda-tools') {
 
 pipelineJob('lambda-tools/adhoc-lambda-test') {
   description('Invoke adhoc-lambda with an EventBridge-style test event')
-
-  // 👇 Embed the pipeline script from the seed job's workspace
   definition {
-    cps {
-      script(readFileFromWorkspace('infra-jenkins/pipelines/test_adhoc_lambda.Jenkinsfile'))
-      sandbox(false)
+    cpsScm {
+      lightweight(false)  // <- force full checkout, no @script
+      scm {
+        git {
+          remote { url('git@github.com:isstephen/infra-jenkins.git'); credentials('github-ssh') }
+          branches('*/main')
+        }
+      }
+      scriptPath('infra-jenkins/pipelines/test_adhoc_lambda.Jenkinsfile') // match exact case in repo
     }
   }
-
   logRotator { daysToKeep(14); numToKeep(50) }
   disabled(false)
 }
